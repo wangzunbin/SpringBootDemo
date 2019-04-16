60860408e555fe7b2de7148705abe31791b11d1b
wang <905192187@qq.com>
2019-04-16 11:21:21 +0800

    jar包方式运行web项目文件上传和访问（核心知识）

--- src/main/java/com/wangzunbin/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/wangzunbin/springbootdemo/SpringbootdemoApplication.java
@@ -2,9 +2,13 @@ package com.wangzunbin.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.MultipartConfigElement;
 
 @RestController
 @SpringBootApplication
@@ -20,4 +24,14 @@ public class SpringbootdemoApplication {
         SpringApplication.run(SpringbootdemoApplication.class, args);
     }
 
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //单个文件最大
+        factory.setMaxFileSize("10240KB"); //KB,MB
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize("1024000KB");
+        return factory.createMultipartConfig();
+    }
 }
