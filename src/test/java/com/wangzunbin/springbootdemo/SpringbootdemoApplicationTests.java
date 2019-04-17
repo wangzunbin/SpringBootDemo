dcb052f6b3876d2036fd4e71c981370e6c160c9d
wang <905192187@qq.com>
2019-04-17 10:15:50 +0800

    @SpringBootTest单元测试实战

--- src/test/java/com/wangzunbin/springbootdemo/SpringbootdemoApplicationTests.java
+++ src/test/java/com/wangzunbin/springbootdemo/SpringbootdemoApplicationTests.java
@@ -1,5 +1,8 @@
 package com.wangzunbin.springbootdemo;
 
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +14,17 @@ public class SpringbootdemoApplicationTests {
 
     @Test
     public void contextLoads() {
+        TestCase.assertEquals(1, 1);
+        System.out.println("contextLoads");
     }
 
+    @Before
+    public void testBefore(){
+        System.out.println("before");
+    }
+
+    @After
+    public void testAfter(){
+        System.out.println("after");
+    }
 }
